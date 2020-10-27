311371bef84eed140be40896a0365588d3915261
seanwilliams <seanymiles@gmail.com>
2020-10-27 15:09:12 -0400

    buttons and editview are created

--- app/src/main/java/edu/temple/webapp/MainActivity.java
+++ app/src/main/java/edu/temple/webapp/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PageControlFragment palletFragment = new PageControlFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.container_1,palletFragment)
+                .commit();
     }
 }
--- app/src/main/java/edu/temple/webapp/PageControlFragment.java
+++ app/src/main/java/edu/temple/webapp/PageControlFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.GridView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +60,8 @@ public class PageControlFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page_control, container, false);
+        View l = inflater.inflate(R.layout.fragment_page_control, container, false);
+        EditText editText = (EditText) l.findViewById(R.id.editText);
+        return l;
     }
 }
